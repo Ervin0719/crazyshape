611464ebe63388111299f68d59b860fadaa6dcfe
Ervin <o1083011@gm.pu.edu.tw>
2021-05-24 11:15:56 +0800

    2021上機考v3_(趙泓斌)

--- app/src/main/java/tw/edu/pu/o1083011/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083011/crazyshape/MainActivity.kt
@@ -1,6 +1,7 @@
 package tw.edu.pu.o1083011.crazyshape
 
 import android.content.Intent
+import android.os.Build.VERSION_CODES.N
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -18,19 +19,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        generate()
+
         val img: ImageView = findViewById(R.id.imgTitle)
         GlideApp.with(this)
             .load(R.drawable.cover)
             .override(800, 600)
             .into(img)
 
-        Toast.makeText(this,"作者 = 趙泓斌",Toast.LENGTH_LONG).show()
-        imgNext.setOnLongClickListener(object: View.OnLongClickListener{
-            override fun onLongClick(p0:View):Boolean{
-                intent = Intent(this@MainActivity,GameActivity::class.java)
+        var X: Int = (1..N).random()
+
+        Toast.makeText(this, "作者 = 趙泓斌", Toast.LENGTH_LONG).show()
+        imgNext.setOnLongClickListener(object : View.OnLongClickListener {
+            override fun onLongClick(p0: View): Boolean {
+                intent = Intent(this@MainActivity, GameActivity::class.java)
                 startActivity(intent)
+                recreate()
                 return true
             }
         })
+    }
+
+
+    private fun generate() {
+        val shape =
+            intArrayOf(R.drawable.circle, R.drawable.square, R.drawable.triangle, R.drawable.star)
+        val i: Int = (0..3).random()
+        imgNext.setImageResource(shape[i])
     }
 }
